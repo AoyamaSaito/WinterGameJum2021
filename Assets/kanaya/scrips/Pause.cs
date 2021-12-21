ffb5a1411837ff62f04167ff922cdc2c0563b15c
kny17s <83321238+kny17s@users.noreply.github.com>
2021-12-21 18:48:23 +0900

    残弾のUIと一時停止ボタンを作成しました。

---
+++ Assets/kanaya/scrips/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PausePanel;
+    private bool inGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inGame = true;
+        PausePanel.SetActive(false);
+    }
+    public void PauseButton()
+    {
+        inGame = false;
+        PausePanel.SetActive(true);
+    }
+    public void Back()
+    {
+        inGame = true;   //すぐに始まってしまう
+        PausePanel.SetActive(false);
+    }
+    public void Retry()
+    {
+
+    }
+    public void Retire()
+    {
+
+    }
+
+}
