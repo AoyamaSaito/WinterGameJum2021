50022bdbc49fdd1bd7e90eb1775d500eca36c98c
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-12-21 13:25:32 +0900

    DoTween

---
+++ Assets/Hijikata/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>ボタンの効果音</summary>
+    [SerializeField] AudioSource _audioClip = default;
+    /// <summary>_gameStartにシーン切り替え</summary>
+    [SerializeField] string _gameStart = default;
+
+    public void GameStart()
+    {
+        _audioClip.Play();
+        SceneManager.LoadScene(_gameStart);
+        Debug.Log("ゲームスタート");
+    }
+
+    public void GameClear()
+    {
+        _audioClip.Play();
+        SceneManager.LoadScene("");
+    }
+}
