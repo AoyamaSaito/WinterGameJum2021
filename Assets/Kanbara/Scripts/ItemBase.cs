f8641d6f1f8bd3ed3d10391cdde327039af4dc4a
Ryusei1115 <tekunikaru22@gmail.com>
2021-12-21 15:30:00 +0900

    nullチェックを追加した

---
+++ Assets/Kanbara/Scripts/ItemBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    [SerializeField, Header("セットするゲームオブジェクト")] GameObject[] _items = default;
+    private int Score => _score;
+
+    public int _score = default;
+
+    void GameStart()
+    {
+        _score = 0;
+    }
+
+    public void ScoreChange(int i)
+    {
+        _score += i;
+    }
+}
