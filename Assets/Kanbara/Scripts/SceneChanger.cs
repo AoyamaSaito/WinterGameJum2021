30863dfea448bc1e64c40a1a9824eaf61ffeb2dd
Ryusei1115 <tekunikaru22@gmail.com>
2021-12-21 18:46:54 +0900

    余計なものを消した

--- Assets/Kanbara/Scripts/SceneChanger.cs
+++ Assets/Kanbara/Scripts/SceneChanger.cs
@@ -2,20 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Threading.Tasks;
-using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    [SerializeField, Header("表示するテキスト")] Text _text = default;
     [SerializeField, Header("切り替えたいシーン名")] string _sceneName = default;
-    [SerializeField, Header("テキストを表示する時間（ミリ秒）")] int _textDelay = default;
     [SerializeField, Header("接触したときに行動を起こすタグ")] string _triggerTag = default;
     AudioSource _audioSource;
 
     void Start()
     {
-        _text.enabled = false;
         _audioSource = GetComponent<AudioSource>();
     }
 
@@ -24,14 +19,9 @@ public class SceneChanger : MonoBehaviour
         
     }
 
-    private async void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag != _triggerTag) return;
-        if(_text != null)
-        {
-            _text.enabled = true;
-            await Task.Delay(_textDelay);
-        }
         _audioSource.Play();
         SceneManager.LoadScene(_sceneName);
     }
