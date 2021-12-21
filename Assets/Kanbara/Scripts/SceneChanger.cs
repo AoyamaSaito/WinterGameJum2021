6bf346dadbbb49203aaa2c61374835fb64cc5ff4
Ryusei1115 <tekunikaru22@gmail.com>
2021-12-21 14:32:11 +0900

    クリア判定とゲームオーバー判定を実装した

---
+++ Assets/Kanbara/Scripts/SceneChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField, Header("表示するテキスト")] Text _text = default;
+    [SerializeField, Header("切り替えたいシーン名")] string _sceneName = default;
+    [SerializeField, Header("テキストを表示する時間（ミリ秒）")] int _textDelay = default;
+    void Start()
+    {
+        _text.enabled = false;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private async void OnTriggerEnter2D(Collider2D collision)
+    {
+        _text.enabled = true;
+        await Task.Delay(_textDelay);
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
