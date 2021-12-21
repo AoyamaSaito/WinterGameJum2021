33e31c28986c3ae2bdf4784be5f18958e2c0be85
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-12-21 15:21:37 +0900

    Fade追加

---
+++ Assets/Hijikata/Fade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Fade : SceneChanger
+{
+    public void Fill()
+    {
+        FadeValue(1f);
+    }
+
+    void FadeValue(float Value)
+    {
+        Camera.main.DOColor(Color.black, 1.5f)
+        .SetEase(Ease.InSine)
+        .OnComplete(() => base.OnClick());
+    }
+}
--- Assets/Hijikata/Gamemanager.cs
+++ Assets/Hijikata/Gamemanager.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Gamemanager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>ボタンの効果音</summary>
+    [SerializeField] AudioSource _audioClip = default;
+
+    /// <summary>Stage1にシーン切り替え</summary>
+    public void Stage1()
     {
-        
+        //_audioClip.Play();
+        SceneManager.LoadScene("Stage1");
+        Debug.Log("Stage1にシーン切り替え");
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>Stage2にシーン切り替え</summary>
+    public void Stage2()
     {
-        
+        //_audioClip.Play();
+        SceneManager.LoadScene("");
+        Debug.Log("Stage2にシーン切り替え");
     }
+
+    /// <summary>Stage3にシーン切り替え</summary>
+    public void Stage3()
+    {
+        //_audioClip.Play();
+        SceneManager.LoadScene("");
+        Debug.Log("Stage3にシーン切り替え");
+    }
+
+    /// <summary>Titleにシーン切り替え</summary>
+    public void Title()
+    {
+        //_audioClip.Play();
+        SceneManager.LoadScene("");
+        Debug.Log("タイトルへ");
+    }
+
+    /// <summary>GameFinish</summary>
+    public void GameFin()
+    {
+        //_audioClip.Play();
+        Application.Quit();
+        Debug.Log("ゲーム終了");
+    }
+
 }
