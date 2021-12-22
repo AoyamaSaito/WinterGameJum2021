ff3f52a6198b0da8a1e685f2f5f8af7aa5cc9ed1
機関車ヘイヘイ\vantan <yu.love5416@gmail.com>
2021-12-22 12:00:21 +0900

    壊れるブロックのscript

--- Assets/Hijikata/BreakBlock.cs
+++ Assets/Hijikata/BreakBlock.cs
@@ -5,43 +5,66 @@ using UnityEngine;
 public class BreakBlock : MonoBehaviour
 {
     /// <summary>オブジェクトをはこ</summary>
-    [SerializeField] GameObject _collsionGameObject = default;
+    GameObject _collsionGameObject = default;
     /// <summary>Blockが壊れるまでの回数</summary>
     [SerializeField] int _breakCount = 3;
+    [SerializeField] float _intervalTime = 1f;
     bool _breakInterval = false;
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Block")
+        if (_collsionGameObject == null)
         {
+            if (collision.gameObject.tag == "Block" && _breakInterval == false)
+            {
+                _collsionGameObject = collision.gameObject;
+                BlockBreak();
+                PlayerAttackBlock();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" && _breakInterval == false)
+        {
+            _breakInterval = true;
+            StartCoroutine(Interval());
             _breakCount--;
 
-            if (_breakCount >= 0) Destroy(this.gameObject);
+            if (_breakCount <= 0) Destroy(this.gameObject);
         }
-        //_collsionGameObject = collision.gameObject;
-        //BlockBreak();
-        //PlayerAttackBlock();
     }
 
     /// <summary>_colsionGameObjectにはいったオブジェクトのタグが"Block"だった時に_breakCountを減らして尚且つ_breakCountが0より小さくなくなった時に壊す</summary>
     void BlockBreak()
     {
-        if (_collsionGameObject.tag == "Block" )
+        if (_collsionGameObject.tag == "Block" && _breakInterval == false)
         {
+            _breakInterval = true;
+            StartCoroutine(Interval());
             _breakCount--;
 
-            if (_breakCount >= 0) Destroy(this.gameObject);
+            if (_breakCount <= 0) Destroy(this.gameObject);
         }
     }
 
     /// <summary>_colsionGameObjectにはいったオブジェクトのタグが"Player"だった時に_breakCountを減らして尚且つ_breakCountが0より小さくなくなった時に壊す</summary>
     public void PlayerAttackBlock()
     {
-        if (gameObject.tag == "Player")
+        if (gameObject.tag == "Player" && _breakInterval == false)
         {
+            _breakInterval = true;
+            StartCoroutine(Interval());
             _breakCount--;
 
-            if (_breakCount >= 0) Destroy(this.gameObject);
+            if (_breakCount <= 0) Destroy(this.gameObject);
         }
+    }
+
+    IEnumerator Interval()
+    {
+        yield return new WaitForSeconds(_intervalTime);
+        _breakInterval = false;
     }
 }
