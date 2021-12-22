2c8e4bd7f034dfd2f3e0cf1cc5416bf812cd3b52
Ryusei1115 <tekunikaru22@gmail.com>
2021-12-22 14:08:27 +0900

    ステージの原型

--- Assets/Kanbara/Scripts/SceneChanger.cs
+++ Assets/Kanbara/Scripts/SceneChanger.cs
@@ -7,27 +7,41 @@ public class SceneChanger : MonoBehaviour
 {
     [SerializeField, Header("切り替えたいシーン名")] string _sceneName = default;
     [SerializeField, Header("接触したときに行動を起こすタグ")] string _triggerTag = default;
+    [SerializeField, Header("表示するパネル(クリア)")] GameObject _clearPanel;
+    [SerializeField, Header("表示するパネル(ゲームオーバー)")] GameObject _gameOverPanel;
+    [SerializeField, Header("")] Bulletlife _bulletLife = default;
+
     AudioSource _audioSource;
 
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        _clearPanel.SetActive(false);
+        _gameOverPanel.SetActive(false);
     }
 
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag != _triggerTag) return;
         _audioSource.Play();
-        SceneManager.LoadScene(_sceneName);
+        _bulletLife._isNotPlayGame = true;
+        Debug.Log("true");
+        _clearPanel.SetActive(true);
+        //SceneManager.LoadScene(_sceneName);
     }
 
     public void OnClick()
     {
         SceneManager.LoadScene(_sceneName);
+    }
+
+    public void GameOver()
+    {
+        _gameOverPanel.SetActive(true);
     }
 }
